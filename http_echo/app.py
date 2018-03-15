3acb9f44267e242d1f6cc141e86dfef9837e0786
Robson Peixoto <robson.peixoto@stone.com.br>
2018-03-15 08:59:52 -0300

    Accept any route and method

--- http_echo/app.py
+++ http_echo/app.py
@@ -5,14 +5,14 @@ from flask_pymongo import PyMongo
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/http_echo")
 
-DEFAULT_METHODS = ["GET", "POST"]
+ALL_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = MONGO_URI
 mongo = PyMongo(app)
 
-
-@app.route("/echo", methods=DEFAULT_METHODS)
+@app.route('/', defaults={'path': ''}, methods=ALL_METHODS)
+@app.route('/<path:path>', methods=ALL_METHODS)
 def echo():
     args = request.args or {}
     body = request.get_json() or {}
